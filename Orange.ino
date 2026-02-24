2d4b95ff12034edd1c5a044f205c41535583bd02
Kevin <149292645+Nutrijelly@users.noreply.github.com>
2026-02-24 19:35:36 +0700

    Add files via upload

---
+++ Orange.ino
@@ -0,0 +1,458 @@
+int p = 1;
+
+  // Frekwensi Buzzer untuk Nada
+  int n1 = 262; int n2 = 294; int n3 = 330; int n4 = 349;
+  int n5 = 392; int n6 = 440; int n7 = 494; int n8 = 523;   //i = 8
+  // Frekwensi Buzzer untuk Nada
+
+  // Ketukan
+  int k1 = 1000; //Ketukan 1
+  int k2 = 2000; //Ketukan 2
+  int k3 = 3000; //Ketukan 3
+  int k4 = 4000; //Ketukan 4
+
+  int ks = 500;
+  // Ketukan
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  tone(p, 0, k2);              delay(k2); // 0
+    tone(p, n3, k1);              delay(k1); 
+    tone(p, 370, ks);              delay(ks); 
+    tone(p, n5, ks);              delay(ks); 
+    tone(p, n7, k1);              delay(k1); 
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n3, k2);              delay(k2);
+
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n1, ks);              delay(ks);
+    tone(p, 247, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+  // tone(p, 0, 10);              delay(10); // 0
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n1, ks);              delay(ks);
+    tone(p, 247, k1);              delay(k1);
+
+
+  // tone(p, 0, k1);              delay(k1); // 0
+    tone(p, 247, ks);              delay(ks); 
+    tone(p, n1, k1);              delay(k1); 
+
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n1, ks);              delay(ks); 
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n1, ks);              delay(ks); 
+    tone(p, n5, ks);              delay(ks); 
+    tone(p, 370, k1);              delay(k1);
+
+
+    tone(p, n3, ks);              delay(ks); 
+    tone(p, n3, k1);              delay(k1); 
+
+    tone(p, n3, ks);              delay(ks);
+
+    tone(p, n2, ks);              delay(ks); 
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n2, ks);              delay(ks); 
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n2, k1);              delay(k1);
+
+
+    tone(p, n6, ks);              delay(ks); 
+    tone(p, n5, k1);              delay(k1); 
+
+
+    tone(p, n6, ks);              delay(ks); 
+    tone(p, n7, ks);              delay(ks); 
+    tone(p, n8, k1);              delay(k1); 
+
+    tone(p, n3, ks);              delay(ks); 
+    tone(p, n5, k1);              delay(k1); 
+
+    tone(p, n3, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks); 
+    tone(p, n3, ks);              delay(ks); 
+    tone(p, n7, ks);              delay(ks); 
+    tone(p, n8, k1);              delay(k1);
+
+    tone(p, n7, ks);              delay(ks); 
+    tone(p, n7, ks);              delay(ks); 
+  // tone(p, 0, 10);              delay(10); // 0
+    tone(p, n6, k1);              delay(k1);
+
+  tone(p, 0, 500);              delay(500); // 0
+
+
+  // tone(p, 0, 10);              delay(10); // 0
+      tone(p, n3, ks);              delay(ks);
+      tone(p, 370, k1);              delay(k1);
+
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n6, ks);              delay(ks);
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n3, k1);              delay(k1);
+
+      tone(p, n2, ks);              delay(k1);
+      tone(p, n2, k1);              delay(k1);
+
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n1, k1);              delay(k1);
+
+      tone(p, n1, ks);              delay(ks);
+      tone(p, 247, ks);              delay(ks);
+      tone(p, n1, ks);              delay(ks);
+      tone(p, n1, ks);              delay(ks);
+      tone(p, 247, k1);              delay(k1);
+
+  tone(p, 0, 500);              delay(500); // 0
+      tone(p, n1, ks);              delay(ks);
+      tone(p, n1, k1);              delay(k1);
+
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, 370, k1);              delay(k1);
+
+
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n6, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n3, ks);              delay(ks);
+      tone(p, 370, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, 370, k1);              delay(k1);
+
+      tone(p, n1, ks);              delay(ks);
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n1, ks);              delay(ks);
+      tone(p, n5, ks);              delay(ks);
+
+
+
+// Pre-Chorus:
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n3, ks);              delay(ks);
+    tone(p, 370, k1);              delay(k1);
+
+
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, 370, k1);              delay(k1);
+
+    tone(p, n5, k1);              delay(k1);
+
+
+  tone(p, 0, 500);              delay(500); // 0
+      tone(p, n3, ks);              delay(ks);
+      tone(p, 370, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n5, ks);              delay(ks);
+      tone(p, 370, ks);              delay(ks);
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n2, ks);              delay(ks);
+      tone(p, n8, k1);              delay(k1);
+
+      tone(p, n8, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n6, ks);              delay(ks);
+      tone(p, n7, k1);              delay(k1);
+
+
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, 370, k1);              delay(k1);
+
+    tone(p, n5, k1);              delay(k1);
+
+
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n3, ks);              delay(ks);
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, 659, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+
+
+// Chorus
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+
+  tone(p, 0, 500);              delay(500); // 0
+      tone(p, n5, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, 659, k1);              delay(k1);
+
+      tone(p, n8, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n8, ks);              delay(ks);
+
+      tone(p, 659, k1);              delay(k1);
+
+      tone(p, 659, ks);              delay(ks);
+      tone(p, 659, ks);              delay(ks);
+      tone(p, 698, ks);              delay(ks);
+      tone(p, 659, ks);              delay(ks);
+
+      tone(p, 370, ks);              delay(ks);
+      tone(p, 370, ks);              delay(ks);
+      tone(p, n5, k1);              delay(k1);
+
+      tone(p, n3, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n6, ks);              delay(ks);
+      tone(p, n6, k1);              delay(k1);
+
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n8, k1);              delay(k1);
+
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n7, ks);              delay(ks);
+      tone(p, n6, k1);              delay(k1);
+
+      tone(p, n6, k1);              delay(k1);
+
+      tone(p, n7, ks);              delay(ks);
+
+      tone(p, n8, k1);              delay(k1);
+
+  tone(1, 0, 500);              delay(500); // 0
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, 659, k1);              delay(k1);
+    tone(p, n7, k1);              delay(k1);
+
+
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, 659, k1);              delay(k1);
+
+    tone(p, n8, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n8, k1);              delay(k1);
+
+    tone(p, 659, k1);              delay(k1);
+    
+    tone(p, 659, ks);              delay(ks);
+    tone(p, 659, ks);              delay(ks);
+    tone(p, 659, ks);              delay(ks);
+
+    tone(p, 698, ks);              delay(ks);
+
+    tone(p, 659, k1);              delay(k1);
+
+    tone(p, 370, ks);              delay(ks);
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n6, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, 370, k1);              delay(k1);
+
+    tone(p, n3, k1);              delay(k1);
+
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+
+// Verse:
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n3, ks);              delay(ks);
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n7, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n3, k1);              delay(k1);
+
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n2, k1);              delay(k1);
+
+    tone(p, n1, k1);              delay(k1);
+
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n7, k1);              delay(k1);
+
+  tone(p, 0, 500);              delay(500); // 0
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n1, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, 370, ks);              delay(ks);
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n2, k1);              delay(k1);
+
+    tone(p, n2, ks);              delay(ks);
+    tone(p, n6, k1);              delay(k1);
+    
+    tone(p, n5, k1);              delay(k1);
+
+    tone(p, n6, ks);              delay(ks);
+    tone(p, n7, ks);              delay(ks);
+    tone(p, n8, k1);              delay(k1);
+
+    tone(p, n3, ks);              delay(ks);
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n4, ks);              delay(ks);
+    tone(p, 370, ks);              delay(ks);
+
+    tone(p, n5, ks);              delay(ks);
+    tone(p, n5, k1);              delay(k1);
+
+  tone(p, 0, 500);              delay(500); // 0
+    
+}
